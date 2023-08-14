963ef5a7d3267b21a8bf8228bbb95f4e98fb31c9
Distant22 <sindistant@gmail.com>
2023-08-14 09:10:20 +0800

    feat(server):add bodyparser and route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const port = 3000
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+app.use(bodyParser.json());
+
 app.get('/',(req, res) => {res.send('WeShare is listening!')})
+
+const user_route = require('./routes/userRoutes');
+app.use('/users',user_route);
 
 
 module.exports = server;
