fedaae2b2bd0c299d0d8ad87e046ecb97b82b567
ycccccccccccc <jessica.ychuang@gmail.com>
2023-08-16 17:22:47 +0800

    Update event API

--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const util = require('../utils/util');
 const eventController = require('../controllers/eventController');
 
-router.get('/', [util.authorize_bearer], eventController);
+router.get('/', [util.authorize_bearer], eventController.getEvent);
 
 module.exports = router;
