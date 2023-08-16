5441033ecc086fcc7c1be793dc0e43d071f3703f
ycccccccccccc <jessica.ychuang@gmail.com>
2023-08-16 11:52:20 +0800

    Order API modified

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,6 @@ module.exports = {
         const item_id = parseInt(req.params.item_id);
         const get_seller = await getSeller(res, item_id);
         const seller_id = get_seller.seller_id;
-        console.log(item_id, seller_id, buyer_id);
         if(buyer_id === seller_id){
             return res.status(400).json({
                 error: "Can't buy own product!"
@@ -20,7 +19,6 @@ module.exports = {
         const seller_id = req.user.id;
         const order_id = parseInt(req.params.order_id);
         const IDs = await orderModel.getIDs(res, order_id);
-        console.log(IDs)
         if( seller_id !== IDs.seller_id){
             return res.status(400).json({
                 error: "Insufficient permissions!"
