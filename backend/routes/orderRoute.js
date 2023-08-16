7f12f0b60fb517b1bd6a82e6bfaa02c84c9d92a1
ycccccccccccc <jessica.ychuang@gmail.com>
2023-08-16 11:17:28 +0800

    Order API modified

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const util = require('../utils/util');
 const orderController = require('../controllers/orderController');
 
-router.post('/:id', [util.authorize_json, util.authorize_bearer], orderController.addOrder);
-router.put('/:id/agree', [util.authorize_json, util.authorize_bearer], orderController.agreeOrder);
+router.post('/:id', [util.authorize_bearer], orderController.addOrder);
+router.put('/:id/agree', [util.authorize_bearer], orderController.agreeOrder);
 
 module.exports = router;
