d79bf45661586662dc12def4dfca5e0ed0a825b6
Distant22 <sindistant@gmail.com>
2023-08-16 10:00:59 +0000

    fix:chat APIs

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -2,21 +2,21 @@ const chatModel = require('../models/chatModel')
 
 module.exports = {
     getMessage: async (req, res) => {
-        const { my_ID } = req.user.id
-        const { seller_ID } = req.params.id;
-        const result = await chatModel.getMessage(my_ID, seller_ID);
+        const my_ID = req.user.id
+        const seller_ID = req.params.id;
+        const result = await chatModel.getMessage(res,my_ID, seller_ID);
         return res.status(200).json({ data: result }); 
     },
     getMessagePreview: async (req, res) => {
-        const { my_ID } = req.user.id
-        const result = await chatModel.getMessagePreview(my_ID);
+        const my_ID = req.user.id
+        const result = await chatModel.getMessagePreview(res,my_ID);
         return res.status(200).json({ data: result }); 
     },
     sendMessage: async (req, res) => {
-        const { my_ID } = req.user.id
-        const { seller_ID } = req.params.id
+        const sender_id = req.user.id
+        const receiver_id = req.params.id
         const { message } = req.body
-        const result = await chatModel.sendMessage(my_ID,seller_ID,message)
+        const result = await chatModel.sendMessage(res,sender_id,receiver_id,message)
         return res.status(200).json({ data: result })
     },
 }
