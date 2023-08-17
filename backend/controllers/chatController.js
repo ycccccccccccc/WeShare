34ad3477aa0a4e0c1bf33a1aaf335bbb5049dd53
Distant22 <sindistant@gmail.com>
2023-08-17 09:55:02 +0800

    feat(chat):add cursor

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -15,22 +15,7 @@ module.exports = {
         const cursor = req.query.cursor;
         const decode_cursor = cursor === undefined ? 0 : Number(Buffer.from(cursor, 'base64').toString('ascii'))
         console.log("decode_cursor: ",decode_cursor)
-        const result = await chatModel.getMessagePreview(res,my_ID,decode_cursor,limit);
-        let base64String = '';
-        if(result.length == (limit + 1)){
-            const last_index = result.length - 1;
-            const next_cursor = {
-                'item_id': result[last_index].id
-            }
-            const jsonString = JSON.stringify(next_cursor);
-            base64String = Buffer.from(jsonString).toString('base64');
-            //刪掉第11篇
-            result.pop();
-        }
-        else{
-            base64String = null;
-        }
-        'next_cursor': base64String
+        const result = await chatModel.getMessagePreview(res,my_ID,decode_cursor);
         return res.status(200).json({ data: result }); 
     },
     sendMessage: async (req, res) => {
