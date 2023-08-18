8ac00c18dbc526fd5874995f1e7c13060e47d067
justinli <kobs666666@gmail.com>
2023-08-18 09:07:18 +0800

    feat: feat/shareapi -v2

---
+++ frontend/weshare/src/app/Share/page.js
@@ -0,0 +1,14 @@
+import CreateObject from "../../../Components/createObject";
+import Navbar from "../../../Components/navbar";
+import "./sharepage.css";
+
+export default function Sharepage() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <CreateObject />
+      </main>
+    </div>
+  );
+}
