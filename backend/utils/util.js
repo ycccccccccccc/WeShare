632d18578033004ae5b19bbf1ed52dd0df1fa2e9
ycccccccccccc <jessica.ychuang@gmail.com>
2023-08-22 15:09:23 +0800

    db connection check

--- backend/utils/util.js
+++ backend/utils/util.js
@@ -9,14 +9,14 @@ const db = mysql.createPool({
     database: process.env.NODE_ENV === 'test' ? 'weshare_test' : 'weshare'
 });
 
-db.getConnection((err, connection) => {
-    if (err) {
-        console.error('Database connection failed:', err);
-    } else {
-        console.log('Database connection successful!');
-        connection.release(); // Release the connection back to the pool
-    }
-});
+// db.getConnection((err, connection) => {
+//     if (err) {
+//         console.error('Database connection failed:', err);
+//     } else {
+//         console.log('Database connection successful!');
+//         connection.release(); // Release the connection back to the pool
+//     }
+// });
 
 module.exports = {
 
