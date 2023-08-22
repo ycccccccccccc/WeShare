d6b75e5f157a7bb278689108ca01c67db3f10400
Distant22 <sindistant@gmail.com>
2023-08-22 09:53:52 +0800

    feat(server):add fan route and rm unneeded import and logs

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const http = require('http');
 const path = require('path');
 const jwt = require('jsonwebtoken')
-const util = require('./utils/util')
 const { db } = require('./utils/util');
 
 app.use(bodyParser.json());
@@ -33,6 +32,9 @@ app.use('/orders', order_route);
 const event_route = require('./routes/eventRoute');
 app.use('/events', event_route);
 
+const fan_route = require('./routes/fanRoute');
+app.use('/fans', fan_route);
+
 const server = http.createServer(app); // Create an HTTP server
 
 const io = require("socket.io")(server, {
@@ -49,46 +51,38 @@ io.use((socket, next) => {
       	return { error: 'No token provided' };
       }
       try {
-	  const accessToken = token.split(' ')[1];
-          // 'WeShare' 之後要移去.env
-          const decoded = jwt.verify(accessToken, 'WeShare');
-	  console.log("Decoded:",decoded)
-          next();
+	      const accessToken = token.split(' ')[1];
+        // 'WeShare' 之後要移去.env
+        const decoded = jwt.verify(accessToken, 'WeShare');
+        console.log("Decoded:",decoded)
+        next();
       } catch (error) {
-          console.log("error:",error)
-          return { error: 'Invalid token' };
+        console.log("error:",error)
+        return { error: 'Invalid token' };
       }
 })
 
 io.on("connection", (socket) => {
   socket.on("message", async (msg) => {
-
-    console.log("connection success,message is",msg,msg.id,msg.message,socket.handshake.headers.authorization)
     const accessToken = socket.handshake.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(accessToken, 'WeShare');
-    const big = decoded.id > msg.id ? decoded.id : msg.id
-    const low = decoded.id > msg.id ? msg.id : decoded.id
-    socket.join(`chat${low}${big}`)
-    console.log(`chat${low}${big}`)
+    const room_ID = decoded.id > msg.id ? `${msg.id}${decoded.id}` : `${decoded.id}${msg.id}`
+    socket.join(`chat${room_ID}`)
+    console.log(`chat${room_ID}`)
     try {
-
     	const sql = "INSERT INTO chat (sender_id, receiver_id, message) VALUES (?, ?, ?)"
     	const [results] = await db.query(sql, [decoded.id,msg.id,msg.message])
     	const data = {
     		id: results.insertId,
-        	message: msg.message,
-		user: {
-			id:decoded.id
-		}
+        message: msg.message,
+        user: {
+          id:decoded.id
+        }
     	}
-    	io.to(`chat${low}${big}`).emit("response",data)
-        console.log("Send success")
+    	io.to(`chat${room_ID}`).emit("response",data)
+      console.log("Send success")
     } catch (err) {
-	console.log("send err:",err)
-      return { 
-        msg: "Socket io error",
-        err: err 
-      };
+	    console.error(err)
     }
   })
 });
