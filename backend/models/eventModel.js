96a04b6534fe5c6a81138a55622f07e06155fee9
ycccccccccccc <jessica.ychuang@gmail.com>
2023-08-22 17:55:06 +0800

    update getEvent

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     getEvent: async (res, id) => {
         try {
-            const sql = 'SELECT event_table.item_id, event_table.id, event_table.type, event_table.sender_id, event_table.recipient_id, event_table.is_read, event_table.created_at, user.name, user.image, order_table.quantity, order_table.status, item.title \
+            const sql = 'SELECT event_table.item_id, event_table.id, event_table.type, event_table.sender_id, event_table.recipient_id, event_table.is_read, DATE_FORMAT(event_table.created_at, "%Y-%m-%d %H:%i:%s") AS created_at, user.name, user.image, order_table.quantity, order_table.status, item.title \
             FROM event_table \
             LEFT JOIN user ON event_table.sender_id = user.id \
             LEFT JOIN item ON event_table.item_id = item.id \
