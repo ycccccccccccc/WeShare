9799f7a6afad1d5aa4a93cbfaba8869462d8ad50
ycccccccccccc <jessica.ychuang@gmail.com>
2023-08-24 17:06:08 +0800

    update API

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,8 +4,8 @@ const util = require('../utils/util');
 const orderController = require('../controllers/orderController');
 
 router.post('/:item_id', [util.authorize_json, util.authorize_bearer], orderController.addOrder);
-router.get('/', [util.authorize_bearer], orderController.getOrders);
 router.get('/:item_id', [util.authorize_bearer], orderController.getItemOrders);
+router.get('/', [util.authorize_bearer], orderController.getOrders);
 router.put('/:order_id/agree', [util.authorize_bearer], orderController.agreeOrder);
 router.delete('/:order_id', [util.authorize_bearer], orderController.delOrder);
 
