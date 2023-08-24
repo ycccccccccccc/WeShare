d6a63f3d0761448c777642524d448cc09f852a90
ycccccccccccc <jessica.ychuang@gmail.com>
2023-08-24 14:49:28 +0800

    Update API

--- backend/models/itemModel.js
+++ backend/models/itemModel.js
@@ -1,6 +1,6 @@
 const util = require('../utils/util')
 const { db } = require('../utils/util');
-const { getUser, getUserInfo } = require('./userModel')
+const { getUserInfo } = require('./userModel')
 
 module.exports = {
 
@@ -16,7 +16,7 @@ module.exports = {
             return util.databaseError(err,'addItem',res);
         }
     },
-    SELECT seller_id FROM item WHERE id: async ( res, id ) => {
+    getSeller: async ( res, id ) => {
         try {
             const sql = 'SELECT seller_id FROM item WHERE id = ?'
             const [results] = await db.query(sql, [id])
